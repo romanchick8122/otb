27889f572d853a1bcec694e84b3b0b2b002aca0f
Raman Svistunou <roma-svistunov@tut.by>
2026-03-07 12:55:03 +0300

    Fix build on some compilers

--- src/core/render/camera_component.cpp
+++ src/core/render/camera_component.cpp
@@ -15,9 +15,9 @@ namespace otb
 {
 CameraComponent::CameraComponent()
 {
-    camera.position = (Vector3){ 5.0f, 5.0f, 5.0f }; // Camera position
-    camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };   // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };       // Camera up vector (rotation towards target)
+    camera.position = { 5.0f, 5.0f, 5.0f }; // Camera position
+    camera.target = { 0.0f, 2.0f, 0.0f };   // Camera looking at point
+    camera.up = { 0.0f, 1.0f, 0.0f };       // Camera up vector (rotation towards target)
     camera.fovy = 90.0f;                             // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;          // Camera projection type
 }
