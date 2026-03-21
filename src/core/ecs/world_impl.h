8452e9d56b338eeb399fede036b3420f8e4f6b24
Raman Svistunou <roma-svistunov@tut.by>
2026-03-21 15:03:43 +0300

    Use npos instead of -1uz

--- src/core/ecs/world_impl.h
+++ src/core/ecs/world_impl.h
@@ -73,7 +73,7 @@ World::ComponentIterator<T> World::components_end()
     if (const auto it = components.find(tid);
         it != components.end())
     {
-        return ComponentIterator<T>(&it->second, -1uz);
+        return ComponentIterator<T>(&it->second, std::string::npos);
     }
     return ComponentIterator<T>(nullptr, 0);
 }
